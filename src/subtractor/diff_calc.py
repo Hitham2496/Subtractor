3166ed6dd203c8cf349a1aeeed4679e2d485333c
Hitham Hassan <hitham.t.hassan@durham.ac.uk>
2022-03-10 17:50:10 +0000

    updates from the talk to diff_calc.py

--- src/subtractor/diff_calc.py
+++ src/subtractor/diff_calc.py
@@ -1,8 +1,16 @@
 #!/usr/bin/env python
 
-def diff(x, y):
+def diff(x, y=0, TOL=9):
     """
     Finds the difference between
-    two numbers.
+    two numbers x and y, default
+    value of y is 0.
     """
-    return x -y
+    if not (isinstance(x, int) or isinstance(x, float)):
+        raise TypeError(
+                "diff accepts only float or int arguments.")
+    if not (isinstance(y, int) or isinstance(y, float)):
+        raise TypeError(
+                "diff accepts only float or int arguments.")
+    return round(x - y, TOL)
+
